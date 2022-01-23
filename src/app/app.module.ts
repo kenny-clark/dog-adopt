ceec5fd2e4e033522803894b58bac586540c6773
Kenny <krc00002@mix.wvu.edu>
2022-01-23 15:38:57 -0500

    added navbar files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { navBarComponent } from 'src/app/navigation/navbar.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    navBarComponent
   ],
   imports: [
     BrowserModule
