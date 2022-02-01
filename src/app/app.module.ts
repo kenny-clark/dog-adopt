51836205a951f15e44e38fcae7a98e4f8e63a720
Kenny <krc00002@mix.wvu.edu>
2022-02-01 13:38:14 -0500

    Added one card that still needs some work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { navBarComponent } from 'src/app/navigation/top-navbar.component';
-
 import { AppComponent } from './app.component';
+import { dogCards } from './cards/dog-cards.component';
 import { Footer1 } from './navigation/footer/footer1.component';
 import { Footer2 } from './navigation/footer/footer2.component';
 
@@ -11,7 +11,8 @@ import { Footer2 } from './navigation/footer/footer2.component';
     AppComponent,
     navBarComponent, 
     Footer1,
-    Footer2
+    Footer2,
+    dogCards
   ],
   imports: [
     BrowserModule
